158c4336afcb6e265c020e0a80c7b204d247008c
abdelrahman atef <102483897+AbdoMan19@users.noreply.github.com>
2023-10-30 22:15:44 +0200

    add a function as a comment in compression class asa backup

--- compression.java
+++ compression.java
@@ -42,3 +42,61 @@ class compression {
         return compressedData;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Vector<LZ77Tag> v = new Vector<>();
+//         int curr = 0;
+
+//         while (curr < data.length()) {
+//             int position = 0;
+//             int maxLength = 0;
+//             char nextSymbol = data.charAt(maxLength + curr);
+//             for (int i = 1; i <= WINDOW_SIZE && i <= curr; i++)
+//             {
+//                 int length = 0;
+
+//                 while (curr + length < data.length() && length < BUFFER_SIZE && curr + length - i < curr
+//                         && data.charAt(curr + length) == data.charAt(curr + length - i))
+//                 {
+//                     length++;
+//                 }
+
+//                 if (length > maxLength)
+//                 {
+//                     position = i;
+//                     maxLength = length;
+//                     if (curr + length < data.length())
+//                         nextSymbol = data.charAt(curr + length);
+//                     else
+//                         nextSymbol = 0;
+//                 }
+//             }
+//             curr += maxLength + 1;
+//             v.add(new LZ77Tag(position, maxLength, nextSymbol));
+// //            System.out.println(position + " " + maxLength + " " + nextSymbol);
+//         }
+//         return  v;
